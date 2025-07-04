0bd1986a10fd52fbde9f522d0e498a0a18b51986
MohsenRefaat <refaatmohsen5@gmail.com>
2025-07-04 21:55:04 +0200

    update ServerRoute

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '', // الصفحة الرئيسية فقط
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'products/:pid',
+    renderMode: RenderMode.Server  // أو NoPrerender
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server // باقي المسارات العشوائية
   }
 ];
